b934855fc2031bb6871f8b5fb47c944e209b4913
Sergon <sgzzsifuentes@gmail.com>
2022-11-11 21:12:35 -0600

    More artifacts

---
+++ Artifacts/Constants.py
@@ -0,0 +1,73 @@
+from VM.Borderlands import Borderland
+
+class C_Table:
+    delimitation = Borderland().area
+
+    c_integers = {}
+    c_floats = {}
+    c_strings = {}
+    c_booleans = {}
+
+
+    def addInteger(self, value: str, addr: int) -> bool:
+        if value not in self.c_integers:
+            self.c_integers[value] = addr
+            return True
+        else:
+            return False
+
+    def getInteger(self, value: str) -> int:
+        return self.c_integers[value]
+
+    def addFloat(self, value: str, addr: int) -> bool:
+        if value not in self.c_floats:
+            self.c_floats[value] = addr
+            return True
+        else:
+            return False
+
+    def getFloat(self, value: str) -> int:
+        return self.c_floats[value]
+
+    def addString(self, value: str, addr: int) -> bool:
+        if value not in self.c_strings:
+            self.c_strings[value] = addr
+            return True
+        else:
+            return False
+
+    def getString(self, value: str) -> int:
+        return self.c_strings[value]
+
+    def addBoolean(self, value: str, addr: int) -> bool:
+        if value not in self.c_booleans:
+            self.c_booleans[value] = addr
+            return True
+        else:
+            return False
+
+    def getBoolean(self, value: str) -> int:
+        return self.c_booleans[value]
+
+    def getCTable(self) -> dict:
+        table = {
+            "integer": self.c_integers,
+            "float": self.c_floats,
+            "string": self.c_strings,
+            "boolean": self.c_booleans
+        }
+        return table
+
+    def printCTable(self):
+        print("Integers:")
+        print(self.c_integers)
+        print()
+        print("Floats:")
+        print(self.c_floats)
+        print()
+        print("Strings:")
+        print(self.c_strings)
+        print()
+        print("Booleans:")
+        print(self.c_booleans)
+        print()
