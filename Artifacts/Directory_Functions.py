bcd4396242b24bd2e9fcc118815c562ea8c5f6ab
Sergon <sgzzsifuentes@gmail.com>
2022-11-15 21:54:15 -0600

    Added some artifacts that I forgot to upload

---
+++ Artifacts/Directory_Functions.py
@@ -0,0 +1,29 @@
+from Artifacts.Functions import Function
+
+class Directory_Func:
+    
+    def __init__(self):
+        self.directory = {}
+
+    def getFunc(self, name: str) -> Function:
+        return self.directory[name]
+
+    def addFunc(self, function: Function):
+        self.directory[str(function.name)] = function
+
+    def getDirectory(self) -> dict:
+        return self.directory
+
+    def showDirectory(self):
+        if self.directory is not None:
+            for func in self.directory:
+                print(self.directory[func])
+                self.directory[func].showParams()
+
+    def hasFunc(self, name: str) -> bool:
+        hasKey = False
+
+        if name in self.directory:
+            hasKey = True
+        
+        return hasKey
---
+++ Artifacts/Functions.py
@@ -0,0 +1,53 @@
+from Artifacts.Variables import Variable
+
+class Function:
+    name: str
+    data_type: str
+    returnVar: bool
+    initQuad: int
+    addr: int
+
+    def __init__(self, name: str, data_type: str = "void"):
+        self.name = name
+        self.data_type = data_type
+
+        self.params = {}
+        self.vars = {}
+        self.initQuad = 0
+        self.returnVar = False
+        self.addr = 0
+
+
+    def addParam(self, var: Variable):
+        if var.name in self.params:
+            print("Parameter already in function")
+        else:
+            self.params[var.name] = var
+
+    def addVar(self, var: Variable):
+        if var.name in self.params:
+            print("Variable already in function")
+        else:
+            self.vars[var.name] = var
+
+    def addIQuad(self, quad: int):
+        self.initQuad = quad
+        
+    def showParams(self):
+        if self.params is not None:
+            for param in self.params:
+                print(param)
+    
+    def showVars(self):
+        if self.vars is not None:
+            for var in self.vars:
+                print(var)
+
+    def getNParams(self) -> int:
+        return len(self.params)
+
+    def __repr__(self) -> str:
+        return f'Function({self.name}, {self.data_type}, with {len(self.params)} args and {len(self.vars)} vars, starting at Quad #{self.initQuad})'
+
+
+    
